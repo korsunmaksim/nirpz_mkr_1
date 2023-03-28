f7551857f89d8513ff8f7034bb67ebc9cc9c52d4
korsunmaksim <99172016+korsunmaksim@users.noreply.github.com>
2023-03-28 13:18:02 +0300

    feat: added main func"

--- main.py
+++ main.py
@@ -34,4 +34,16 @@ def check_diff_elems(arr_1,arr_2):
     return diff
 
 
-    
+def main(input_path_1,input_path_2,same_path,diff_path):
+    strs_1=get_data(input_path_1)
+    strs_2=get_data(input_path_2)
+    same=check_same_elems(strs_1,strs_2)
+    diff=check_diff_elems(strs_1,strs_2)
+    for elem in check_diff_elems(strs_2, strs_1):
+        diff.append(elem)
+    write_into_file(same_path,same)
+    write_into_file(diff_path,diff)
+    print(same,diff)
+
+
+main("input1.txt","input2.txt","same.txt","diff.txt")
