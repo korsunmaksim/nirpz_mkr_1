7f49520a66a6e8e74decc1edf168dcb2d4c9063b
korsunmaksim <99172016+korsunmaksim@users.noreply.github.com>
2023-03-28 13:15:23 +0300

    feat: added write data and check same functions

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
+#Get info from files
 def get_data(file_path):
     info = []
     file=open(file_path,'r')
-    for str in file:
-        info.append(str)
+    for line in file:
+        line=line.replace("\n","")
+        info.append(line)
     file.close()
     return info
+
+#Write info into file
+def write_into_file(file_path:str,info):
+    file = open(file_path, "w")
+    file.truncate()
+    for line in info:
+        file.write(line + "\n")
+    file.close()
+
+#Check same elements
+def check_same_elems(arr_1,arr_2):
+    same= []
+    for elem in arr_1:
+        if elem in arr_2:
+            same.append(elem)
+    return same
+
+
+#Check diff elements
+def check_diff_elems(arr_1,arr_2):
+    diff= []
+    for elem in arr_1:
+        if elem not in arr_2:
+            diff.append(elem)
+    return diff
 
 
     
